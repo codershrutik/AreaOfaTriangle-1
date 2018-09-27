0dca7d7f8a5f0228087fb3683618e66c15bc040c
aemal4075 <aemal4075@7JHN6Y1.Student.UGDSB.ED>
2018-09-27 10:08:36 -0400

    Area of a triangle for U2A1

---
+++ src/areaofarectangle/AreaOfARectangle.java
@@ -0,0 +1,30 @@
+/*
+ * Aedrianne Malawis
+ * AreaOfRectangle.java
+ * This program calculates the area of 5.7 by 4.8 rectangle
+ * September 18, 2018
+ */
+
+package areaofarectangle;
+
+/**
+ *
+ * @author aemal4075
+ */
+public class AreaOfARectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double width = 4.8;
+        double length = 5.7;
+        double area = 0.0;
+        // algorithm for area calc
+        area = width * length;
+        System.out.println("the area ..." + area + " cm squared");
+
+        // TODO code application logic here
+    }
+    
+}
